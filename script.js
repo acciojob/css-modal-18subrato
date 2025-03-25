0f0e93782866a14c94a294c04aa5b3b9db5516f4
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-25 17:53:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ let openDiv = document.querySelector('#openModal')
 let modal = document.querySelector(".modal");
 let closeDiv = document.querySelector('.close-modal')
 
+function closeModal(){
+	modal.style.display = 'none';
+}
+
 openDiv.addEventListener('click',()=>{
-	modal.style.display = 'block'
+	modal.style.display = 'block';
 })
 
-closeDiv.addEventListener("click",()=>{
-	modal.style.display = 'none'
+closeDiv.addEventListener("click",closeModal);
+
+window.addEventListener('click',(e)=>{
+	if(e.target === modal){
+		closeModal();
+	}
 })
-
-
-window.addEventListener("click", (event) => {
-    if (!modal.contains(event.target) && event.target !== modal && event.target !== openDiv) {
-        modal.style.display = 'none';
-    }
-});
